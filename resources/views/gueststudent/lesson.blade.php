6f1028a59259a4034440c3ecfc3c96800411253e
air <asilkhan-al@mail.ru>
2019-01-11 22:43:22 +0600

    fixed

--- resources/views/gueststudent/lesson.blade.php
+++ resources/views/gueststudent/lesson.blade.php
@@ -37,7 +37,17 @@
                             <span class="text-muted small">урок</span>
                             <p class="text-dark m-0 text-center">{{$lesson->name}}</p>
                         </div>
-                        <div class="card-body" id="lessonPart">
+                        <div class="container-fluid">
+                            <div class="text-center">
+                                <a class="btn btn-success btn-sm my-2 float-left" id="btnLeft"><span class="fa fa-arrow-left text-white"></span></a>
+                                <span id='countdowntimer' class='text-muted text-center'></span>
+                                <a class="btn btn-success btn-sm my-2 float-right" id="btnRight"><span class="fa fa-arrow-right text-white"></span></a>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="card-body" id="lessonPart">
+
+                                </div>
+                            </div>
 
                         </div>
                         <div class="card-footer">
@@ -56,6 +66,13 @@
     <script src="{{ URL::asset('assets/js/validator.min.js')}}"></script>
     <script>
         $(document).ready(function () {
+
+            $('#btnRight').click(function () {
+                alert('Right');
+            });
+            $('#btnLeft').click(function () {
+                alert('Left');
+            });
             var currentLessonPartId = {{$currentLessonPart->id}};
 
             constructFrame("{{$lessonPart->presentation}}",
@@ -72,7 +89,21 @@
                     timeleft =  parseInt(timeleft);
                 }
 
-                var form = "<p id='countdowntimer' class='text-muted text-center'></p>";
+                var form = "";
+
+                if (audio != null) {
+                    if (audio.length > 0) {
+                        form += "                            <div class=\"card my-1\">\n" +
+                            "                                <div class=\"card-body\">\n" +
+                            "                                    <audio controls autoplay='autoplay'>\n" +
+                            "                                        <source src=\"/" + audio + "\">\n" +
+                            "                                        Your browser does not support the audio tag.\n" +
+                            "                                    </audio>\n" +
+                            "                                </div>\n" +
+                            "                            </div>";
+                    }
+                }
+
                 form += "<div class=\"embed-responsive embed-responsive-16by9\">\n" +
                     "                                <iframe class=\"embed-responsive-item\" id=\"viewer\"\n" +
                     "                                        src=\"/assets/ViewerJS/#/" + presentation + "\" allowfullscreen\n" +
@@ -91,18 +122,7 @@
                             "                            </div>\n";
                     }
                 }
-                if (audio != null) {
-                    if (audio.length > 0) {
-                        form += "                            <div class=\"card my-1\">\n" +
-                            "                                <div class=\"card-body\">\n" +
-                            "                                    <audio controls>\n" +
-                            "                                        <source src=\"/" + audio + "\">\n" +
-                            "                                        Your browser does not support the audio tag.\n" +
-                            "                                    </audio>\n" +
-                            "                                </div>\n" +
-                            "                            </div>";
-                    }
-                }
+
                 var downloadTimer = setInterval(function(){
                     timeleft--;
                     document.getElementById("countdowntimer").textContent = timeleft;
