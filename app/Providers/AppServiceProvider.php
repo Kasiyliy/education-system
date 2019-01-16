c234554a77bcd2e458dfb60dad1021a72e1a6dca
air <asilkhan-al@mail.ru>
2019-01-17 01:29:44 +0600

    PRESENTATION

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //Schema::defaultStringLength(191);
         view()->composer('layouts.master', 'App\Http\Composers\MasterComposer');
     }
 
