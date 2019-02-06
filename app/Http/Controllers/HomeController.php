f84a789f26b4e0cd9dbd6d7f59f3511aa40bbea3
air <asilkhan-al@mail.ru>
2019-02-07 01:30:37 +0600

    Send email check

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,6 +34,11 @@ class HomeController extends Controller
         return view('home', compact('error', 'institute'));
     }
 
+    public function mail()
+    {
+        return view('mail');
+    }
+
     public function lock()
     {
         $user = auth()->user();
