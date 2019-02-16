5e6528a19d34b2fbdfe6465ded760e090a903c2d
madibani <akpan.madi@gmail.com>
2019-02-16 21:21:52 +0600

    linechart delete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,35 +34,5 @@
 @section('extrascript')
 <script src="{{ URL::asset('assets/js/Chart.min.js')}}"></script>
   <!-- Chart.js -->
-  <script>
-    Chart.defaults.global.legend = {
-      enabled: false
-    };
-    // Line chart
-    var ctx = document.getElementById("lineChart");
-    var lineChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        datasets: [{
-          label: "Income",
-          backgroundColor: "rgba(38, 185, 154, 0.31)",
-          borderColor: "rgba(38, 185, 154, 0.7)",
-          pointBorderColor: "rgba(38, 185, 154, 0.7)",
-          pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointBorderWidth: 1,
-        }, {
-          label: "Expence",
-          backgroundColor: "rgba(3, 88, 106, 0.3)",
-          borderColor: "rgba(3, 88, 106, 0.70)",
-          pointBorderColor: "rgba(3, 88, 106, 0.70)",
-          pointBackgroundColor: "rgba(3, 88, 106, 0.70)",
-          pointHoverBackgroundColor: "#fff",
-          pointHoverBorderColor: "rgba(151,187,205,1)",
-          pointBorderWidth: 1,
-        }]
-      },
-    });
-</script>
+
 @endsection
